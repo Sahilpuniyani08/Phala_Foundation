5a54c9f3388cb5a15511b39982fa6abe8f39bdd8
Uttamg61001 <uttamg61001.com>
2024-04-17 13:25:30 +0530

    Navbar Add

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
+import './App.css'
+import Navbar from './components/Navbar'
+function App() {  
   return (
-    <>
-      
-      <h1 className='heading'>Vite + React</h1>
-     
-    
+    <> 
+      <Navbar/>  
     </>
   )
 }
